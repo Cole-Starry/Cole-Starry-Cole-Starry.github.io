d3150fc60796e6e81c01d4d53111cac912d709f8
Cole Starry <500202429@fvtc.edu>
2026-02-18 15:50:08 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,30 @@
-console.log(document.querySelector('h1'));
+
+
+const Welcome = document.querySelector('#Welcome')
+
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const divWel = document.createElement('div')
+Welcome.append(divWel)
+
+var html;
+
+if(isMorning){
+html = '<div>Good Morning</div>';
+}
+else if(isAfternoon){
+ html = '<div>Good Afternoon</div>';
+}
+else if(isEvening){ 
+html = '<div>Good Evening</div>';
+}
+else{
+ html = '<div>Error</div>';
+}
+
+divWel.innerHTML = html;
